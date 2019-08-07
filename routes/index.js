5bdf0f3da66655dcd8ebd805922334ad6dbe805f
edujastre <edujastre@yahoo.com.br>
2019-08-07 14:56:02 -0300

    build corrections

--- routes/index.js
+++ routes/index.js
@@ -10,10 +10,60 @@ let environment = new paypal.core.SandboxEnvironment(clientId, clientSecret);
 let client = new paypal.core.PayPalHttpClient(environment);
 
 
+// CREATE PAYMENT JSON
+const createPaymentJson = ({
+    carrinho,
+    valor,
+    descricao
+}) => ({
+    "intent": "sale",
+    "payer": {
+        "payment_method": "paypal"
+    },
+    "redirect_urls": {
+        "return_url": "http://localhost:3000/success",
+        "cancel_url": "http://localhost:3000/cancel"
+    },
+    "transactions": [{
+        "item_list": {
+            "items": carrinho
+        },
+        "amount": valor,
+        "description": descricao
+    }]
+});
+
+const executePaymentJson = ({
+    payerId,
+    valor
+}) => ({
+    "payer_id": payerId,
+    "transactions": [{
+        "amount": valor
+    }]
+});
+
 const products = require('../config/products').products;
 
+// ROUTES - PAGE
+router.get('/', (req, res) => res.render('index', {
+    products
+}));
+
 // ROUTES WITH PAYPAL
+let globalProductSelected;
 router.post('/buy', (req, res) => {
+    const productId = req.query.id;
+    
+    console.log(`request: ${JSON.stringify(req.body)}`);
+    const product = products.reduce((all, p) => {
+        // console.log(p.id === productId ? p : all);
+        return p.id === productId ? p : all
+    });
+    // console.log('produto =>', product);
+    if (!product.id) return res.render('index', {
+        products
+    });
 
     let request = new paypal.orders.OrdersCreateRequest();
     request.headers["prefer"] = "return=representation";
@@ -37,6 +87,9 @@ router.post('/buy', (req, res) => {
         // If call returns body in response, you can get the deserialized version from the result attribute of the response.
         console.log(`Order: ${JSON.stringify(order.result)}`);
 
+
+
+
         res.status(200).json({
             orderID: order.result.id
         });
@@ -46,11 +99,54 @@ router.post('/buy', (req, res) => {
 
 });
 
+// ROUTER DE RESPOSTA DE PAGAMENTO COM SUCESSO
+router.get('/successc', (req, res) => {
+    res.send('success'); // criar ejs
+});
+
+router.get('/success', (req, res) => {
+    console.log(req);
+    const payerId = req.query.id;
+    const paymentId = req.query.paymentId;
+    const valor = {
+        "currency": "BRL",
+        "total": globalProductSelected.preco.toFixed(2) // SEM DB
+    };
+    const execute_payment_json = executePaymentJson({
+        payerId,
+        valor
+    });
+
+    paypal.payment.execute(paymentId, execute_payment_json, (error, payment) => {
+        if (error) {
+            console.log(error.response);
+            throw error; // criar ejs
+        } else {
+            console.log('Get Payment Response');
+            console.log(JSON.stringify(payment));
+            // res.send('Success'); // criar ejs
+            res.render('success', {
+                payment
+            })
+        }
+    });
+});
+
+// ROUTER DE RESPOSTA DE PAGAMENTO CANCELADO
+router.get('/cancel', (req, res) => {
+    res.send('Cancelled'); // criar ejs
+});
+
 router.get('/products/:id?', (req, res) => {
 
     const productId = req.params.id;
     const product =  productId ? products.filter(item => item.id == productId) : products;
     
+    // products.reduce((all, p) => {
+    //     console.log(productId, p.id === productId ? p : all);
+    //     return p.id === productId ? p : all
+    // });
+
     res.status(200).json(product)
 })
 
