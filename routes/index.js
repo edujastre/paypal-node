abbe5eaf0117d99df069a6fb564119520fc9ba30
edujastre <edujastre@yahoo.com.br>
2019-08-07 15:35:25 -0300

    Heroku configuration

--- routes/index.js
+++ routes/index.js
@@ -9,40 +9,6 @@ let clientSecret = "ED2DmYZ2OzwQPZN2T0b72rHgrDbnNCE0EAZngVktBv1eL3DWRjIf8nuXtues
 let environment = new paypal.core.SandboxEnvironment(clientId, clientSecret);
 let client = new paypal.core.PayPalHttpClient(environment);
 
-
-// CREATE PAYMENT JSON
-const createPaymentJson = ({
-    carrinho,
-    valor,
-    descricao
-}) => ({
-    "intent": "sale",
-    "payer": {
-        "payment_method": "paypal"
-    },
-    "redirect_urls": {
-        "return_url": "http://localhost:3000/success",
-        "cancel_url": "http://localhost:3000/cancel"
-    },
-    "transactions": [{
-        "item_list": {
-            "items": carrinho
-        },
-        "amount": valor,
-        "description": descricao
-    }]
-});
-
-const executePaymentJson = ({
-    payerId,
-    valor
-}) => ({
-    "payer_id": payerId,
-    "transactions": [{
-        "amount": valor
-    }]
-});
-
 const products = require('../config/products').products;
 
 // ROUTES - PAGE
@@ -51,28 +17,19 @@ router.get('/', (req, res) => res.render('index', {
 }));
 
 // ROUTES WITH PAYPAL
-let globalProductSelected;
 router.post('/buy', (req, res) => {
     const productId = req.query.id;
-    
-    console.log(`request: ${JSON.stringify(req.body)}`);
-    const product = products.reduce((all, p) => {
-        // console.log(p.id === productId ? p : all);
-        return p.id === productId ? p : all
-    });
-    // console.log('produto =>', product);
-    if (!product.id) return res.render('index', {
-        products
-    });
 
+    console.log(`request: ${JSON.stringify(req.body)}`);
+ 
     let request = new paypal.orders.OrdersCreateRequest();
     request.headers["prefer"] = "return=representation";
     request.requestBody(req.body);
 
-       // Call API with your client and get a response for your call
+    // Call API with your client and get a response for your call
     let createOrder = async function () {
         let order;
-        try {            
+        try {
             order = await client.execute(request);
         } catch (err) {
 
@@ -87,9 +44,6 @@ router.post('/buy', (req, res) => {
         // If call returns body in response, you can get the deserialized version from the result attribute of the response.
         console.log(`Order: ${JSON.stringify(order.result)}`);
 
-
-
-
         res.status(200).json({
             orderID: order.result.id
         });
@@ -100,37 +54,10 @@ router.post('/buy', (req, res) => {
 });
 
 // ROUTER DE RESPOSTA DE PAGAMENTO COM SUCESSO
-router.get('/successc', (req, res) => {
+router.get('/success', (req, res) => {
     res.send('success'); // criar ejs
 });
 
-router.get('/success', (req, res) => {
-    console.log(req);
-    const payerId = req.query.id;
-    const paymentId = req.query.paymentId;
-    const valor = {
-        "currency": "BRL",
-        "total": globalProductSelected.preco.toFixed(2) // SEM DB
-    };
-    const execute_payment_json = executePaymentJson({
-        payerId,
-        valor
-    });
-
-    paypal.payment.execute(paymentId, execute_payment_json, (error, payment) => {
-        if (error) {
-            console.log(error.response);
-            throw error; // criar ejs
-        } else {
-            console.log('Get Payment Response');
-            console.log(JSON.stringify(payment));
-            // res.send('Success'); // criar ejs
-            res.render('success', {
-                payment
-            })
-        }
-    });
-});
 
 // ROUTER DE RESPOSTA DE PAGAMENTO CANCELADO
 router.get('/cancel', (req, res) => {
@@ -140,12 +67,7 @@ router.get('/cancel', (req, res) => {
 router.get('/products/:id?', (req, res) => {
 
     const productId = req.params.id;
-    const product =  productId ? products.filter(item => item.id == productId) : products;
-    
-    // products.reduce((all, p) => {
-    //     console.log(productId, p.id === productId ? p : all);
-    //     return p.id === productId ? p : all
-    // });
+    const product = productId ? products.filter(item => item.id == productId) : products;
 
     res.status(200).json(product)
 })
