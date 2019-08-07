fb0877ed0a1038e086fad90122deabbd46c036e5
edujastre <edujastre@yahoo.com.br>
2019-08-07 15:47:50 -0300

    heroku port

--- server.js
+++ server.js
@@ -13,4 +13,10 @@ app.use(bodyParser.json());
 
 app.use('/', require('./routes'));
 
-app.listen(3000, () => console.log('Server running on 3000'));
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
+
+// app.listen(process.env.PORT || 3000, () => console.log('Server running on 3000'));
